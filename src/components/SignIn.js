19ac64fdce0746dce222d328ca7e450c502243f8
hannesxc <chakravortyaditya@gmail.com>
2022-10-14 17:58:07 +0530

    env vars

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,31 +1,35 @@
 import React, { useEffect, useContext } from 'react';
 import Auth from './Auth';
 import firebase from 'firebase/compat/app';
-import Navbar from './Navbar';  
+import Navbar from './Navbar';
 import { onAuthStateChanged } from 'firebase/auth';
 import { AuthContext } from '../Contexts/AuthContext';
 
 export default function SignIn() {
-  const { setUser} = useContext(AuthContext);
+  const { setUser } = useContext(AuthContext);
   const firebaseConfig = {
-    apiKey: "AIzaSyBzIuFxBQA6n87HO8l_DfuST3gkyqsVubE",
-    authDomain: "todo-app-authv2.firebaseapp.com",
-    projectId: "todo-app-authv2",
-    storageBucket: "todo-app-authv2.appspot.com",
-    messagingSenderId: "845849550740",
-    appId: "1:845849550740:web:ac476a92923e9a34597981",
-    measurementId: "G-EBDZ7QP82B"
+    apiKey: process.env.REACT_APP_TODO_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_TODO_FIREBASE_AUTH_DOM,
+    projectId: process.env.REACT_APP_TODO_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_TODO_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_TODO_FIREBASE_MSG_SENDER_ID,
+    appId: process.env.REACT_APP_TODO_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_TODO_FIREBASE_MEASUREMENT_ID
   };
   firebase.initializeApp(firebaseConfig);
 
   useEffect(() => {
     const unsub = onAuthStateChanged(firebase.auth(), (currentUser) => {
+      try {
         setUser(currentUser);
-        console.log(currentUser);
-      });
-      return () => {
-        unsub();
+      } catch(error) {
+        console.log(error);
       }
+      console.log(currentUser);
+    });
+    return () => {
+      unsub();
+    }
   }, []);
 
   return (
